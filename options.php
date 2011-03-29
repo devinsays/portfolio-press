05fee0d88c4e6b22c4451b3414b08b9ebeb6863a
Devin Price <devin@wptheming.com>
2011-03-29 09:24:06 -0500

    Updated image path in options.php

--- options.php
+++ options.php
@@ -22,7 +22,7 @@ function optionsframework_option_name() {
 function optionsframework_options() {
 		
 	// If using image radio buttons, define a directory path
-	$imagepath =  get_template_directory_uri . '/images/';
+	$imagepath =  get_bloginfo('template_url') . '/images/';
 	
 	// Options array	
 	$options = array();
