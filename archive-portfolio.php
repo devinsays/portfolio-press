b1534240454588ca685198aee311a473dfbe30c8
Devin Price <devin@wptheming.com>
2011-05-14 13:50:04 -0500

    Update to changelog.

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -13,14 +13,7 @@ Template Name: Portfolio
 			<div id="portfolio">
 
 				<?php
-				// WP 3.0 PAGED BUG FIX
-				if ( get_query_var('paged') )
-					$paged = get_query_var('paged');
-				elseif ( get_query_var('page') ) 
-					$paged = get_query_var('page');
-				else 
-					$paged = 1;
-				//$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 				$args = array( 'post_type' => 'portfolio',
 							   'posts_per_page' => 9,
 							   'paged' => $paged ); 
