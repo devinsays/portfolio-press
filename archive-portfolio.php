5822f83c6636312fb5f1bb9bfbac190b248c3f05
Devin Price <devin@wptheming.com>
2011-05-23 18:39:32 -0500

    Fixes for paging bug when portfolio is set as home page.

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -12,12 +12,19 @@ Template Name: Portfolio
 
 			<div id="portfolio">
 
-				<?php
-				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-				$args = array( 'post_type' => 'portfolio',
-							   'posts_per_page' => 9,
-							   'paged' => $paged ); 
-				query_posts($args);		
+			<?php
+			// WP 3.0 PAGED BUG FIX
+			if ( get_query_var('paged') )
+				$paged = get_query_var('paged');
+			elseif ( get_query_var('page') )
+				$paged = get_query_var('page');
+			else
+				$paged = 1;
+			
+			$args = array( 'post_type' => 'portfolio',
+						   'posts_per_page' => 9,
+						   'paged' => $paged ); 
+			query_posts($args);		
 			?>
             <?php if (have_posts()) : $count = 0; ?>
             <?php while (have_posts()) : the_post(); $count++; global $post; ?>
