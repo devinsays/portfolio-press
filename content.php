2d80fb7c250120376844bc29e2301344fce8c0f0
Devin Price <devin@wptheming.com>
2012-01-07 15:21:37 -0600

    Image and gallery post formats.

--- content.php
+++ content.php
@@ -8,7 +8,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'portfoliopress' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
 		<div class="entry-meta">
 			<?php
 				printf( __( '<span class="meta-prep meta-prep-author">Posted on </span><a href="%1$s" rel="bookmark"><time class="entry-date" datetime="%2$s" pubdate>%3$s</time></a> <span class="meta-sep"> by </span> <span class="author vcard"><a class="url fn n" href="%4$s" title="%5$s">%6$s</a></span>', 'portfoliopress' ),
--- index.php
+++ index.php
@@ -31,6 +31,7 @@ get_header(); ?>
 			<?php endif; ?>
 
 			</div><!-- #content -->
+		</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
