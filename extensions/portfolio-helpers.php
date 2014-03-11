37262efc7ba0516901c9a3804b8f19b22a4cf7cd
Devin Price <devin@wptheming.com>
2014-03-11 16:31:23 -0500

    Simplify pre_get_posts.

--- extensions/portfolio-helpers.php
+++ extensions/portfolio-helpers.php
@@ -11,27 +11,23 @@
  */
 function portfoliopress_template_chooser( $template ) {
 
-	// The 'portfolio_view' query var is set in portfoliopress_portfolio_posts.
-	if ( get_query_var( 'portfolio_view' ) ) {
-		$template = get_query_template( 'archive-portfolio' );
-	}
-
-	return $template;
-}
-add_filter( 'template_include', 'portfoliopress_template_chooser' );
-
-/**
- * Sets posts displayed per portfolio to 9
- *
- * @param object query
- */
-function portfoliopress_portfolio_posts( $query ) {
-
-	if ( !$query->is_main_query() )
-        return;
-
+	global $wp_query;
 	$portfolio = false;
 
+	// Check if the taxonomy query contains only image or gallery post formats
+	if ( is_category() || is_tag() || is_home() ) {
+		$portfolio = true;
+		if ( $wp_query->have_posts() ) :
+			while ( $wp_query->have_posts() ) : $wp_query->the_post();
+				$format = get_post_format();
+				if ( ( $format !=='image' ) && ( $format != 'gallery' ) ) {
+					$portfolio = false;
+				}
+			endwhile;
+		endif;
+	}
+
+	// Check if template should be displayed as archive-portfolio.php
 	if (
 		is_post_type_archive( 'portfolio' ) ||
 		is_tax( 'post_format', 'post-format-image' ) ||
@@ -40,36 +36,57 @@ function portfoliopress_portfolio_posts( $query ) {
 		is_tax( 'portfolio_tag' )
 	) {
 		$portfolio = true;
-		$query->set( 'portfolio_view', true );
 	}
 
-	// Check for $post to avoid notices on 404 page
-	if ( isset( $post) && (
-			is_page_template( 'templates/portfolio.php' ) ||
-			is_page_template( 'templates/full-width-portfolio.php' ) ||
-			is_page_template( 'templates/image-gallery-formats.php' ) ||
-			is_page_template( 'templates/full-width-image-gallery-formats.php' )
-		)
+	// Use the archive-portfolio.php template
+	if ( $portfolio ) {
+		$wp_query->set( 'portfolio_view', true );
+		$template = get_query_template( 'archive-portfolio' );
+	}
+
+	return $template;
+}
+add_filter( 'template_include', 'portfoliopress_template_chooser' );
+
+/**
+ * Default to 9 items displayed per page
+ *
+ * @param object query
+ */
+function portfoliopress_portfolio_posts( $wp_query ) {
+
+	$posts_per_page = get_option( 'posts_per_page', '10' );
+
+	// If user has intentionally set $posts_per_page to something
+	// other than default, we won't override.
+	if ( $posts_per_page == '10' ) {
+		$posts_per_page = apply_filters( 'portfoliopress_posts_per_page', '9' );
+		$wp_query->set( 'posts_per_page', $posts_per_page );
+	}
+
+}
+add_action( 'pre_get_posts', 'portfoliopress_portfolio_posts' );
+
+/**
+ * Adds a body class to archives that display as a portfolio view
+ *
+ * @param array classes applied to post
+ * @return array modified classes
+ */
+function portfoliopress_body_class( $classes ) {
+
+	global $post;
+
+	if (
+		is_page_template( 'templates/portfolio.php' ) ||
+		is_page_template( 'templates/full-width-portfolio.php' ) ||
+		is_page_template( 'templates/image-gallery-formats.php' ) ||
+		is_page_template( 'templates/full-width-image-gallery-formats.php' ) ||
+		get_query_var( 'portfolio_view' )
 	) {
-		$portfolio = true;
-	}
-
-	// Check if the taxonomy query contains only image or gallery post formats
-	if ( is_category() || is_tag() || is_home() ) {
-		$portfolio_view = true;
-		global $wp_query;
-		if ( $wp_query->have_posts() ) :
-			while ( $wp_query->have_posts() ) : $wp_query->the_post();
-				$format = get_post_format();
-				if ( ( $format != 'image' ) && ( $format != 'gallery' ) ) {
-					$portfolio_view = false;
-				}
-			endwhile;
-		endif;
-		// If $portfolio_view false, not all posts were image or gallery
-		if ( $portfolio_view ) {
-			$portfolio = true;
-			$query->set( 'portfolio_view', true );
+		$classes[] = 'portfolio-view';
+		if ( of_get_option( 'portfolio_sidebar', false ) ) {
+			$classes[] = 'full-width-portfolio';
 		}
 	}
 
@@ -84,8 +101,8 @@ function portfoliopress_portfolio_posts( $query ) {
 		)
 	) {
 		foreach( $classes as $key => $value) {
-			if ( $value == 'page-template-templatesfull-width-php') {
-				$classes[$key] = 'page-template-full-width-php';
+			if ( $value == 'page-template-templatesfull-width-page-php') {
+				$classes[$key] = 'page-template-full-width-page-php';
 			}
 			if ( $value == 'page-template-templatesportfolio-php') {
 				$classes[$key] = 'page-template-portfolio-php';
@@ -99,36 +116,6 @@ function portfoliopress_portfolio_posts( $query ) {
 			if ( $value == 'page-template-templatesfull-width-image-gallery-formats-php') {
 				$classes[$key] = 'page-template-full-width-image-gallery-formats-php';
 			}
-		}
-	}
-
-	// If this is a portfolio display, alter posts_per_page
-	if ( $portfolio ) {
-		$posts_per_page = apply_filters( 'portfoliopress_posts_per_page', of_get_option( 'portfolio_num', '9' ) );
-		$query->set( 'posts_per_page', $posts_per_page );
-	}
-
-}
-add_action( 'pre_get_posts', 'portfoliopress_portfolio_posts' );
-
-/**
- * Adds a body class to archives that display as a portfolio view
- *
- * @param array classes applied to post
- * @return array modified classes
- */
-function portfoliopress_body_class( $classes ) {
-
-	if (
-		is_page_template( 'templates/portfolio.php' ) ||
-		is_page_template( 'templates/full-width-portfolio.php' ) ||
-		is_page_template( 'templates/image-gallery-formats.php' ) ||
-		is_page_template( 'templates/full-width-image-gallery-formats.php' ) ||
-		get_query_var( 'portfolio_view' )
-	) {
-		$classes[] = 'portfolio-view';
-		if ( of_get_option( 'portfolio_sidebar', false ) ) {
-			$classes[] = 'full-width-portfolio';
 		}
 	}
 
