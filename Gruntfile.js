2454b5a491ff66320dc27c5110deca90d09f32c7
Devin Price <devin@wptheming.com>
2014-04-21 13:47:01 -0500

    Use Grunt.  Generate .pot file.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+    	// https://www.npmjs.org/package/grunt-wp-i18n
+	    makepot: {
+	        target: {
+	            options: {
+	                domainPath: '/languages/',    // Where to save the POT file.
+	                potFilename: 'theme.pot',   // Name of the POT file.
+	                type: 'wp-theme'  // Type of project (wp-plugin or wp-theme).
+	            }
+	        }
+	    }
+
+	});
+
+	// load tasks
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+
+	// register tasks
+    grunt.registerTask('default', [
+        'makepot'
+    ]);
+
+};
