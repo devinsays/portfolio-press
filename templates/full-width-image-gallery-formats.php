d6c032d04677ed7f2c00d44a43e4dc79c0120a8c
Devin Price <devin@wptheming.com>
2014-10-01 11:23:19 -0500

    Update placeholders to match actual image sizes.

--- templates/full-width-image-gallery-formats.php
+++ templates/full-width-image-gallery-formats.php
@@ -26,33 +26,49 @@ $args = array(
 	),
 	'paged' => $paged
 );
-// Override the primary post loop
-query_posts( $args );
+$portfolio = new WP_Query( $args );
+$thumbnail = 'thumbnail-fullwidth';
 ?>
 
-	<div id="primary">
-		<div id="content" role="main">
+<div id="primary">
+	<div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
+		<?php if ( $portfolio->have_posts() ) : ?>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+			<?php /* Start the Loop */ ?>
+			<?php while ( $portfolio->have_posts() ) : $portfolio->the_post(); ?>
 
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<div class="entry-content">
+						<a href="<?php the_permalink() ?>" rel="bookmark" class="thumb">
+							<h3><?php the_title() ?></h3>
+							<?php if ( has_post_format() ) :
+								$format = get_post_format();
+							?>
+							<div class="portfolio-format-meta icon-format-<?php echo $format; ?>"></div>
+							<?php endif; ?>
+							<?php if ( post_password_required() ) { ?>
+								<img src="<?php echo esc_url( get_template_directory_uri() . '/images/protected-' . $thumbnail . '.gif' ); ?>">
+							<?php }
+							elseif ( has_post_thumbnail() ) {
+								the_post_thumbnail( 'portfolio-' . $thumbnail );
+							} else { ?>
+								<img src="<?php echo esc_url( get_template_directory_uri() . '/images/placeholder-' . $thumbnail . '.gif' ); ?>">
+							<?php } ?>
+						</a>
+					</div><!-- .entry-content -->
+				</article><!-- #post-<?php the_ID(); ?> -->
 
-					<?php
-						get_template_part( 'content', 'portfolio' );
-					?>
+			<?php endwhile; ?>
 
-				<?php endwhile; ?>
+			<?php portfoliopress_paging_nav( $portfolio ); ?>
 
-				<?php portfoliopress_paging_nav(); ?>
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
 
-			<?php else : ?>
-				<?php get_template_part( 'content', 'none' ); ?>
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php wp_reset_query(); ?>
 <?php get_footer(); ?>
